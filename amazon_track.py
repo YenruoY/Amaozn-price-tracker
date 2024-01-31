ac1859857dd592c93849cc6e6e95c2affa4c0a1a
Yenruo <yenruo@tutanota.com>
2024-01-31 22:45:28 +0530

    Minor updates and readme fix

--- amazon_track.py
+++ amazon_track.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from helper_fucntions import get_name
 from helper_fucntions import get_price
@@ -12,7 +13,15 @@ urls = [
         ""]
 
 # Get the price using the function
-for url in urls:
+for url_raw in urls:
+
+  # url pre-processing
+  if url_raw[-1] != '/':
+    url_raw = url_raw + '/'
+
+  # strip the required parts from the url
+  product_id = re.findall(r"/dp/(.+?)/", url_raw)[0]
+  url = "https://www.amazon.in/dp/" + product_id
   
   response = requests.get(url, headers= {'User-agent' : 'Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion'})
 
